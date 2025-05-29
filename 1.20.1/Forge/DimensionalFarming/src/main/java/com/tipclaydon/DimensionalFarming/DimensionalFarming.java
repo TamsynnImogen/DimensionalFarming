946ceed5ccc5181d5dd4e3e6c069b7022d33f65f
TamsynnImogen <apocalypse_unleashed@hotmail.co.uk>
2025-05-29 23:37:04 +0100

    Add files via upload

---
+++ 1.20.1/Forge/DimensionalFarming/src/main/java/com/tipclaydon/DimensionalFarming/DimensionalFarming.java
@@ -0,0 +1,88 @@
+package com.tipclaydon.DimensionalFarming;
+
+import com.mojang.logging.LogUtils;
+import com.tipclaydon.DimensionalFarming.blocks.ModBlocks;
+import com.tipclaydon.DimensionalFarming.blocks.StrippableUtil;
+import com.tipclaydon.DimensionalFarming.items.ModItems;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Item.Properties;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+
+@Mod(DimensionalFarming.MODID)
+public class DimensionalFarming {
+
+    public static final String MODID = "dimensionalfarming";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+
+    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block",
+            () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
+
+    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block",
+            () -> new BlockItem(EXAMPLE_BLOCK.get(), new Properties()));
+
+    public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item",
+            () -> new Item(new Properties()
+                    .food(new net.minecraft.world.food.FoodProperties.Builder()
+                            .alwaysEat().nutrition(1).saturationMod(2f).build())));
+
+    public DimensionalFarming(FMLJavaModLoadingContext context) {
+        IEventBus modEventBus = context.getModEventBus();
+
+        // Register everything
+        ModBlocks.register(modEventBus);
+        ModItems.register(modEventBus);
+        ModCreativeTabs.register(modEventBus);
+
+        BLOCKS.register(modEventBus);
+        ITEMS.register(modEventBus);
+
+        modEventBus.addListener(this::commonSetup);
+        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public static void onCommonSetup(FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            StrippableUtil.registerStrippable(ModBlocks.BLOODBARK_STEM.get(), ModBlocks.STRIPPED_BLOODBARK_STEM.get());
+            StrippableUtil.registerStrippable(ModBlocks.BLOODBARK_HYPHAE.get(), ModBlocks.STRIPPED_BLOODBARK_HYPHAE.get());
+        });
+    }
+    
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        LOGGER.info("HELLO from server starting");
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
+}
